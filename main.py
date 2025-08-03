5ec89be55cb302ac9ca4d049fb0c340e72b519a9
rostyslav333 <rostysla.mois@icloud.com>
2025-08-03 22:09:24 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import time
+import requests
+
+def run_bot():
+    while True:
+        print("Elios AI bot is running...")
+        # Тут буде логіка бота (підключення до API, аналіз, trade)
+        time.sleep(60)
+
+if name == "__main__":
+    run_bot()
